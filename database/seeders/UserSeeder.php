3be94fb84bc1c18e646e88b6896b86e1156b72bc
Ayatooo <l.reynard@isi-dsi.fr>
2025-03-27 13:38:13 +0100

    :zap: develop - seeder modified => mattgones implemented

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -34,6 +34,24 @@ class UserSeeder extends Seeder
             ]);
         });
 
+        $mattgones = User::create([
+            'name' => 'MattGones',
+            'email' => 'matdinville@gmail.com',
+            'password' => Hash::make('password'),
+            'avatar' => null,
+        ]);
+
+        $plants = Plant::factory(random_int(5, 10))->create([
+            'user_id' => $mattgones->id
+        ]);
+
+        $plants->each(function ($plant) use ($mattgones) {
+            Task::factory(random_int(3, 6))->create([
+                'plant_id' => $plant->id,
+                'user_id' => $mattgones->id
+            ]);
+        });
+
         User::factory(10)->create()->each(function ($user) {
             $plants = Plant::factory(random_int(5, 25))->create([
                 'user_id' => $user->id
