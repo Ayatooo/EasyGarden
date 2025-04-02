a77f81d04ed526584cf5735a6c2bd7b970a40c64
Ayatooo <louisreynard919@gmail.com>
2025-04-02 19:32:01 +0200

    :lipstick: develop - banner colors updated

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -4,7 +4,7 @@
         <flux:breadcrumbs.item>Forum</flux:breadcrumbs.item>
     </flux:breadcrumbs>
 
-    <div class="bg-green-50 border-t border-b border-green-300 text-green-600 px-4 py-3 mt-5" role="alert">
+    <div class="bg-green-50 border-t border-b border-green-300 text-green-600 px-4 py-3 rounded rounded-lg mt-5" role="alert">
         <p class="font-bold">Forum 🧿</p>
         <p class="text-sm">
             Vous pouvez poser des questions, répondre à des questions, et discuter de tout ce qui concerne votre jardin.
